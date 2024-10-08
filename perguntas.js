4e9fafaf4948293e714073ac3d232e0ffdec58dd
dudinha1617 <150068152+dudinha1617@users.noreply.github.com>
2024-10-08 12:18:31 -0300

    08/10/24

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+'Programação',
+' O que é JavaScript',
+'O javaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    ' O que é JavaScript',
+    'O javaScript é uma linguagem de programação'
+    )
+
+   criaCartao(
+        'Geografia',
+        ' Qual a capital da França?',
+        'A capital da França é Paris'
+        )
+
+      criaCartao(
+            'Programação',
+            'O que é uma função? ',
+            'Uma função é um bloco de códigos que executa uma função'
+            )
+
+            criaCartao(
+                'Matemática',
+                ' Qual é o resultado 6 x 8?',
+                'O resultado é 48'
+                )
